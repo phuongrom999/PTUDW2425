b4938f612ec135fc2409c331c2e5e36317653860
phuongrom999 <nguyenthanhphuongltdn@gmail.com>
2025-05-02 22:12:44 +0700

    tao database

--- index.js
+++ index.js
@@ -17,6 +17,13 @@ app.engine('hbs',expressHandlebars.engine({
 }));
 app.set('view engine','hbs');
 //route
+app.get('/createTables',(req,res)=>{
+    let models = require('./models');
+    models.sequelize.sync().then(()=>{
+        res.send('tables created!')
+    })
+})
+
 app.get('/',(req,res)=>{
     res.render('index');
 });
